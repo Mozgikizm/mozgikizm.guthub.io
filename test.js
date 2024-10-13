2a74a0d96026832127f5d49d8e6628cb1ed48c41
lazeramisglaz <znznsdmzms@gmail.com>
2024-10-13 15:04:55 +0300

    lol

--- test.js
+++ test.js
@@ -13,18 +13,24 @@ function calculateResult() {
     }
 
   
+
+   
+ 
+ 
+
     let resultText = '';
-    if (score >= 25) {
+    if (score >= 30) {
+        resultText = 'ТАКИХ ДАУНОВ МИР ЕЩЁ НЕ ВИДЕЛ!';
+    } else if (score >= 25) {
         resultText = 'ДА ТЫ ПОЛНЫЙ ДАУН!';
     } else if (score >= 10) {
         resultText = 'Не даун, но интеллект сравним с 8 летним ребенком!';
-    } else if (score >=1) {
+    } else if (score >= 1) {
         resultText = 'НА ДРИПЕ, НЕ ДАУН';
-    
     } else {
         resultText = 'ДА ТЫ НИХУЯ НЕ ВЫБРАЛ ЖЕ!';
     }
 
-  
-    document.getElementById('result').innerText = `Твой результат:  ${resultText}`;
+
+    document.getElementById('result').innerText = `Твой результат: ${resultText}`;
 }
